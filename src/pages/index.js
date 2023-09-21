17982a9754de53616ffddcd1c4b60e4823fb9bdc
Wendy Wang <wendyrackley@10-18-222-96.dynapool.wireless.nyu.edu>
2023-09-21 09:45:58 -0400

    second change

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Data from "../app/components/data";
+import ArticleCard from "../app/components/ArticleCard";
+import styles from "../app/page.module.css";
+
+export default function Home() {
+  const projectName = "Exercise Two";
+
+
+  return (
+    <main className={styles.page}>
+      <h1>{projectName}</h1>
+      {Data.map((article) => (
+      <ArticleCard
+        key={article.id}
+        date={new Date(article.publishedDate).toDateString()}
+        description={article.blurb}
+        imageAlt={article.image.alt}
+        imageSrc={article.image.url}
+        title={article.title}
+        />
+      ))}
+
+    </main>
+  );
+}
